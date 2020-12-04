118bc388fe7a5781049c89379890884c56d1f310
Michael Thomas <m-thomas89@hotmail.com>
2020-12-03 20:25:11 -0500

    src

---
+++ src/src/yourdata.js
@@ -0,0 +1,206 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Michael Thomas",
+  headerTagline: [
+    //Line 1 For Header
+    "Building digital",
+    //Line 2 For Header
+    "products, brands,",
+    //Line 3 For Header
+    "and experience",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "I have built this portfolio so that you may get a glimpse of some of the things I have learned.",
+
+  //Contact Email
+  contactEmail: "m-thomas89@hotmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "Password Generator", //Project Title - Add Your Project Title Here
+      para:
+        "If you are need help generating a password, here you go!", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "https://m-thomas89.github.io/hw3/",
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Weather Dashboard", //Project Title - Add Your Project Title Here
+      para:
+        "If you need to find out the weather for a city you are visiting or in, here you go!", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "https://m-thomas89.github.io/WeatherApp/",
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "BoozieNight", //Project Title - Add Your Project Title Here
+      para:
+        "An app that will help you pick a movie as well as pair your movie choice with an alcoholic beverage", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "https://idallas93.github.io/BootcampProjectOne/",
+    },
+    {
+      id: 4, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Four", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      id: 5, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Five", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      id: 6, //DO NOT CHANGE THIS (Please)😅
+      title: "Progressive Budget", //Project Title - Add Your Project Title Here
+      para:
+        "This app will help you budget.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "https://stark-everglades-13129.herokuapp.com/",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+    ,{
+        id: 5,
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "I was born and raised in Queens, New York. I went to University of Michigan where i got my bachlor's degree in Business Administration and a minor in Biology. My goal was to run my own dental school so I went Barry University in Miami where I got my Master's in Biomedical Science.",
+  aboutParaTwo:
+    "While I was in Miami I found a sales job and started my journey in selling websites for digital marketing agencies.",
+  aboutParaThree:
+    "When I moved back to New York I was in charge of running the sales department of a agency that worked with large brands. My goal is to learn how to code websites and tie that in with my selling abilities.",
+  aboutImage:
+    "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      id: 1,
+      img: htmlIcon,
+      para:
+        "Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.",
+    },
+    {
+      id: 2,
+      img: cssIcon,
+      para:
+        "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.",
+    },
+    {
+      id: 3,
+      img: jsIcon,
+      para:
+        "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.",
+    },
+    {
+      id: 4,
+      img: reactIcon,
+      para:
+        "React is an open-source, front end, JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.",
+    },
+    {
+      id: 5,
+      img: designIcon,
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    },
+    {
+      id: 6,
+      img: codeIcon,
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Heading",
+  promotionPara:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/chetanverma16" },
+    {
+      img: codepenIcon,
+      url: "https://www.codepen.com/",
+    },
+    {
+      img: dribbbleIcon,
+      url: "https://dribbble.com/chetanverma",
+    },
+    {
+      img: instagramIcon,
+      url: "https://www.instagram.com/",
+    },
+  ],
+
+}
+
